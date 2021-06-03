68afa496eeb43af8d1f9944db427158c5394ebb1
hoanghung3011 <65234777+hoanghung3011@users.noreply.github.com>
2021-06-03 16:48:17 +0800

    1

---
+++ baitap/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,n,dem=0;
+		cin>>n;
+		for (i=2;i<n,i++)
+		 { r=n%i;
+			if (r=0) dem++;
+		¯}
+		if (dem>0) cout<<"La so nguyen to" else cout<<"Khong phai so nguyen to";
+}
